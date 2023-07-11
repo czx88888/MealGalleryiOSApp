032effcbd2c6e24e8eb23d912e40637510396470
Zixuan Chen <zixuan77777@gmail.com>
2023-07-10 20:24:47 -0500

    Initial Commit

---
+++ fetchDessertsApp/fetchDessertsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchDessertsAppApp.swift
+//  fetchDessertsApp
+//
+//  Created by Zixuan Chen on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct fetchDessertsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
